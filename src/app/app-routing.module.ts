8b306fea727b381dd6d44af113abb429c31739e4
CienZZZ <Krzysztofkan@poczta.fm>
2020-05-06 09:24:52 +0200

    rename folders to +companies and +contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },//, canActivate: [AuthGuard] },
-  { path: 'companies', loadChildren: () => import('./companies/companies.module').then(m => m.CompaniesModule)},
+  { path: 'companies', loadChildren: () => import('./+companies/companies.module').then(m => m.CompaniesModule)},
   // { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule)},
   // { path: 'activities', loadChildren: () => import('./activities/activities.module').then(m => m.ActivitiesModule)},
   // { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
